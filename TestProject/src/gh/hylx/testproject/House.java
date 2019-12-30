99fd6c2f6489697c5675698dd9990118a3635051
TimLee <TimLee@Miao>
2019-12-29 22:27:17 -0600

    Setup a class: House

---
+++ TestProject/src/gh/hylx/testproject/House.java
@@ -0,0 +1,15 @@
+package gh.hylx.testproject;
+
+public class House {
+	int id;
+	int street_no;
+	String street_name;
+	String city;
+	String state;
+	String nation;
+	int zip;
+	String apartment_name;
+	float square_ft;
+	int floors;
+	
+}
